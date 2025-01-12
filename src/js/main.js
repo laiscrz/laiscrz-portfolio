06695ef4e0b9665cc655ab23afaf845d24fe2950
laiscrz <lais.albino2003@gmail.com>
2025-01-12 14:44:34 -0300

    feat: added about section

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import { initScrollReveal } from "./scrollReveal.js";
 import { typeWrite } from "./typeWrite.js";
 import { menu } from "./menu.js";
+import "./tabs.js";
 
 menu();
 initScrollReveal();
